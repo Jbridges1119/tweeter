b7d60fe56652a019d13c93d355d95a39049f92df
Jeff Bridges <jbridges1119@gmail.com>
2022-06-21 13:41:22 +0000

    Add new tweet body in css and html.

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+var morgan = require('morgan')
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+app.use(morgan('dev'));
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
@@ -29,9 +31,15 @@ require("./lib/date-adjust")();
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
-
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+// Mount the tweets routes at the "/tweets" path prefix:
+app.use("/tweets", tweetsRoutes);
+
+app.post("/tweets", (req,res) => {
+
+})
+
+
