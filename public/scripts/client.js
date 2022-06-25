a4a4e4e964939cc7e89ab3d6e1f6e5fc2068465b
Jeff Bridges <jbridges1119@gmail.com>
2022-06-25 15:55:00 +0000

    Refactor code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,20 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//LOADS WHEN INDEX.HTML IS READY
+$(document).ready(function() {
+
+  loadTweets();
+
+  //`WRITE A NEW TWEET` BUTTON
+  $(".open").click(tweetSlideButton);
+
+  //SUBMIT NEW TWEET BUTTON -- IF OVER 140 OR EMPTY - THROW ERROR MESSAGE
+  $("form").submit(submitNewTweet);
+});
+
+
+
 
 //CONVERTS TEXT INPUT INTO SAFE TEXT -- FOR .HTML CHANGE .HTML TO .TEXT
 const escape = function(str) {
@@ -13,79 +27,80 @@ const escape = function(str) {
 
 //CREATES TWEET USING INFO FROM AN OBJECT
 const createTweetElement = function(data) {
-  const tweet = `<article class="oldTweet">
-  <header>
-    <div class="user"><img src="${data.user.avatars}">${escape(data.user.name)}</div>
-    <div class="userID">${escape(data.user.handle)}</div>
-  </header>
-  <div class="tweet">
-    <p>${escape(data.content.text)}</p>
-  </div>
-  <footer>
+  const tweet =
+  `<article class="oldTweet">
+    <header>
+      <div class="user">
+        <img src="${data.user.avatars}">${escape(data.user.name)}
+      </div>
+      <div class="userID">${escape(data.user.handle)}</div>
+    </header>
+    <div class="tweet">
+      <p>${escape(data.content.text)}</p>
+    </div>
+   <footer>
     <p>${timeago.format(data.created_at)}</p>
     <div class="icons">
-    <i class="fa-solid fa-flag"></i>
-    <i class="fa-solid fa-retweet"></i>
-    <i class="fa-solid fa-heart"></i>
-  </div>
-  </footer>
-</article>`;
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+  </article>`;
   return tweet;
 };
 
 //LOOPS THOUGH AN OBJECT AND FEEDS INFO TO CALLBACK - PREPENDS DATA WITH CALLBACK RETURN
 const renderTweets = function(data) {
+  $(`#tweets-container`).empty();
   data.forEach(person => {
     $('#tweets-container').prepend(createTweetElement(person));
   });
 };
 
-
-//LOADS WHEN INDEX.HTML IS READY
-$(document).ready(function() {
-  //LOADS TWEET HISTORY FROM "/TWEETS"
-  const loadTweets = function() {
-    $.ajax({
-      url: '/tweets',
-      type: 'GET',
-      dataType: 'json',
-      success: function(res) {
-        renderTweets(res);
-      }
-    });
-  };
-  loadTweets();
-
-  //`WRITE A NEW TWEET` BUTTON
-  $(".open").click(function(event) {
-    event.preventDefault();
-    $("html").animate({ scrollTop: 0}, 'fast');
-    $(`.new-tweet`).slideToggle(250);
-    $('#tweet-text').focus();
+//LOADS TWEET HISTORY FROM "/TWEETS"
+const loadTweets = function() {
+  $.getJSON('/tweets', function(res) {
+    renderTweets(res);
   });
+};
 
-  //SUBMIT NEW TWEET BUTTON -- IF OVER 140 OR EMPTY - THROW ERROR MESSAGE
-  $("form").submit(function(event) {
-    event.preventDefault();
-    $(`.alert`).slideUp(75);
-    if ($(this).children('#tweet-text').val().length > 140) {
-      $(".counter").effect("bounce", {times:3, distance: 25}, 400);
-      $(`.alertspan`).text(`Character count cannot be over 140. Currently at ${$(this).children('#tweet-text').val().length}.`);
-      $(`.alert`).slideDown(350);
-    } else if (!$(this).children('#tweet-text').val().length) {
-      $(".counter").effect("bounce", {times:3, distance: 25}, 400);
-      $(`.alertspan`).text('Tweet cannot be empty.');
-      $(`.alert`).slideDown(350);
-    } else {
-      $.post("/tweets", $(this).serialize())
-        .then(() => {
-          loadTweets();
+//`WRITE A NEW TWEET` BUTTON
+const tweetSlideButton = function(event) {
+  event.preventDefault();
+  $("html").animate({ scrollTop: 0}, 'fast');
+  $(`.new-tweet`).slideToggle(250);
+  $('#tweet-text').focus();
+};
+
+const submitNewTweet = function(event) {
+  event.preventDefault();
+  $(`.alert`).slideUp(75);
+  if ($(this).children('#tweet-text').val().length > 140) {
+    $(".counter").effect("bounce", {times:3, distance: 25}, 400);
+    $(`.alertspan`).text(`Character count cannot be over 140. Currently at ${$(this).children('#tweet-text').val().length}.`);
+    $(`.alert`).slideDown(350);
+  } else if (!$(this).children('#tweet-text').val().length) {
+    $(".counter").effect("bounce", {times:3, distance: 25}, 400);
+    $(`.alertspan`).text('Tweet cannot be empty.');
+    $(`.alert`).slideDown(350);
+  } else {
+    $.post("/tweets", $(this).serialize())
+      .then(() => {
+      //   // loadTweets();
+      //   // $('#tweet-text').val("");
+      //   // $('output.counter').text(140 - $('#tweet-text').val().length).css('color', '#545149');
+        $.getJSON('/tweets', function(res) {
+          $('#tweets-container').prepend(createTweetElement(res[res.length - 1 ]));
           $('#tweet-text').val("");
           $('output.counter').text(140 - $('#tweet-text').val().length).css('color', '#545149');
-        })
-        .catch((error) => {
-          console.error(error);
         });
-    }
-  });
-});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+};
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-  //SET CHARACTER COUNTER TO NUMBER/COLOUR ON PAGE REFRESH WITH TEXT ALREADY PRESENT
-  const loadCount = function() {
-    const $length = $('#tweet-text').val().length;
-    if ($length < 141) {
-      $('output.counter').text(140 - $length).css('color', '#545149');
-    } else {
-      $('output.counter').text(140 - $length).css('color', 'red');
-    }
-  };
+  //DOC.READY SHOULD ONLY HAVE INVOKE FUNCTIONS AND LISTENERS -- THIS IS ASYNC SO RUNS LAST
   loadCount();
   //UPDATE CHARACTER COUNTER IN REAL TIME
-  $('#tweet-text').on('input', function() {
-    const $length = $('#tweet-text').val().length;
-    if ($length < 141) {
-      $(this).parents(".new-tweet").find(".counter").text(140 - $length).css('color', '#545149');
-    } else {
-      $(this).parents(".new-tweet").find(".counter").text(140 - $length).css('color', 'red');
-    }
-  });
+  $('#tweet-text').on('input', counter)
 
 });
 
+const counter = function() {
+  const $length = $('#tweet-text').val().length;
+  const $location = $(this).closest(".new-tweet").find(".counter").text(140 - $length)
+  if ($length < 141) {
+    $location.css('color', '#545149');
+  } else {
+    $location.css('color', 'red');
+  }
+};
 
+
+//SET CHARACTER COUNTER TO NUMBER/COLOUR ON PAGE REFRESH WITH TEXT ALREADY PRESENT
+const loadCount = function() {
+  const $length = $('#tweet-text').val().length;
+  if ($length < 141) {
+    $('output.counter').text(140 - $length).css('color', '#545149');
+  } else {
+    $('output.counter').text(140 - $length).css('color', 'red');
+  }
+};
 
