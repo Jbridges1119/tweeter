e7f641393441ec8933f15de1b057c03d613b50c9
Jeff Bridges <jbridges1119@gmail.com>
2022-06-25 16:06:14 +0000

    Refactor code

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   //MOVES WINDOW TO TOP OF PAGE
-  $(`.scrollTop`).click(function(event) {
-    event.preventDefault();
-    $("html").animate({ scrollTop: 0}, 'fast');
-  });
+  $(`.scrollTop`).click(toTop);
 
   //FADES IN/OUT SCROLLTOP BUTTON
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 80) {
-      $('.hidden').fadeIn();
-    } else {
-      $('.hidden').fadeOut();
-    }
-  });
+  $(window).scroll(toTopFade);
 });
+
+//MOVES WINDOW TO TOP OF PAGE
+const toTop = function(event) {
+  event.preventDefault();
+  $("html").animate({ scrollTop: 0}, 'fast');
+};
+
+//FADES IN/OUT SCROLLTOP BUTTON
+const toTopFade = function() {
+  if ($(this).scrollTop() > 80) {
+    $('.hidden').fadeIn();
+  } else {
+    $('.hidden').fadeOut();
+  }
+};
